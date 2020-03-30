a51c7f2503ff4c51dd8fb3c8369db879d4a751d1
Daniel Birmajer <dbirmajer@gmail.com>
2020-03-30 17:21:48 -0400

    Added sortedArrays_tests.py

---
+++ sortedArrays.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+"""
+  This module implement functions for sorted Arrays
+"""
+
+def index(sarr: List, e: int) -> int:
+    q = len(sarr)
+    if q == 0:
+        return None
+    pos = q // 2
+    m = sarr[pos]
+    print("pos = {}, m = {}".format(pos, m))
+    if m == e:
+        return pos
+    elif m > e:
+        return index(sarr[:pos], e)
+    else:
+        try:
+            return pos + 1 + index(sarr[pos+1:], e)
+        except:
+            return None
+
+def elem(e: int, sarr: List) -> bool:
+    a = 0
+    b = len(sarr) - 1
+    q = b - a
+    while (q > 1):
+        pos = q // 2
+        if sarr[pos] == e:
+            return True
+        elif sarr[pos] > e:
+            b = pos - 1
+        else:
+            a = pos + 1
+        q = b - a
+    return False
+    return (sarr[a] == e or sarr[b] == e)
+            
+
+
+
---
+++ sortedArrays_tests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import unittest
+from sortedArrays import elem
+
+class Test_elem(unittest.TestCase):
+    def test_oddLength(self):
+        ls = [0,1,2]
+        self.assertTrue(elem(0, ls))
+        self.assertTrue(elem(1, ls))
+        self.assertTrue(elem(2, ls))
+        self.assertFalse(elem(-1, ls))
+        self.assertFalse(elem(3, ls))
+
+    def test_evenLenght(self):
+        ls = [0,1]
+        self.assertTrue(elem(0, ls))
+        self.assertTrue(elem(1, ls))
+        self.assertFalse(elem(2, ls))
+        self.assertFalse(elem(-1, ls))
+
+    def test_singleton(self):
+        ls = [0]
+        self.assertTrue(elem(0, ls))
+        self.assertFalse(elem(1, ls))
+        self.assertFalse(elem(-1, ls))
+
+    def test_empty(self):
+        ls = []
+        self.assertFalse(elem(1, ls))
+        self.assertFalse(elem(-1, ls))
+        
+
+        
+        
+        
+        
+
+# Run the tests
+unittest.main()    
